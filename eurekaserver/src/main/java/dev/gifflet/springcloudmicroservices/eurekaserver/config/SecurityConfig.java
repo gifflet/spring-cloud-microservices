933e429efc86df33a99ea06faf311f406a401ced
gifflet <guisousa09@hotmail.com>
2024-01-18 23:12:18 -0300

    feat: include security in discovery server (Eureka)

---
+++ eurekaserver/src/main/java/dev/gifflet/springcloudmicroservices/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package dev.gifflet.springcloudmicroservices.eurekaserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
